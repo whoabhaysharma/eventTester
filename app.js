4747f65ec5ff44edb0bacb1b2935213af4ed8051
abhaysharma2719 <whoabhaysharma@gmail.com>
2024-05-05 12:05:45 +0530

    fixed

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 import express from 'express'
-import cron from 'node-cron'
 import { fetchDataAndNotify } from './test.js'
 
 const app = express()
 app.listen(3000, () => {
-    console.log("LISTENING")
+    setInterval(
+        fetchDataAndNotify,
+        30000
+    )
 })
-
-cron.schedule('*/30 * * * * *', async () => {
-    await fetchDataAndNotify();
-});
