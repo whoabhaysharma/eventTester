b33dc45e22b794836cccb9fb29b17f2991980cef
abhaysharma2719 <whoabhaysharma@gmail.com>
2024-05-05 22:18:54 +0530

    fixed the error

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 import express from 'express'
 import { fetchDataAndNotify } from './test.js'
+import cron from 'node-cron'
 
 const app = express()
 app.listen(3000, () => {
     setInterval(
         fetchDataAndNotify,
-        30000
+        60000
     )
 })
+
+app.get('/', (req, res) => {
+    // Send a response back to the client
+    res.send('Hello, World!');
+});
+
+cron.schedule('*/5 * * * *', async () => {
+    fetch("https://eventtester.onrender.com/")
+        .then(response => {
+            console.log("Refreshing.......")
+            if (response.ok) {
+                return response.json();
+            }
+        })
+        .then(data => {
+            // Log or use the response data
+            // console.log(data);
+        })
+        .catch(error => {
+            // Handle any errors that occurred during the fetch request
+            // console.error('Error:', error);
+        });
+});
--- test.js
+++ test.js
@@ -4,29 +4,33 @@ import dotenv from 'dotenv';
 
 dotenv.config()
 
-export const fetchDataAndNotify = async() => {
-    const browser = await puppeteer.launch({
-        headless: true,
-        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH
-    });
-    const page = await browser.newPage();
+export const fetchDataAndNotify = async () => {
+    try {
+        const browser = await puppeteer.launch({
+            headless: true,
+            executablePath: process.env.PUPPETEER_EXECUTABLE_PATH
+        });
+        const page = await browser.newPage();
 
-    // Navigate to a page
-    await page.goto('https://bseh.org.in/all-results');
+        // Navigate to a page
+        await page.goto('https://bseh.org.in/all-results');
 
-    // Wait for a certain amount of time (e.g., 5 seconds)
-    const elementLength = await page.evaluate(() => {
-        // Replace 'your-selector' with your actual CSS selector
-        const elements = document.querySelectorAll('.entry-content ul > li');
-        // Convert NodeList to Array and return
-        return Array.from(elements).length;
-    });
+        // Wait for a certain amount of time (e.g., 5 seconds)
+        const elementLength = await page.evaluate(() => {
+            // Replace 'your-selector' with your actual CSS selector
+            const elements = document.querySelectorAll('.entry-content ul > li');
+            // Convert NodeList to Array and return
+            return Array.from(elements).length;
+        });
 
-    console.log("CHECKED THE RESULT", elementLength)
+        console.log("CHECKED THE RESULT", elementLength)
 
-    if (elementLength > 3) {
-        sendNotification()
+        if (elementLength > 3) {
+            sendNotification()
+        }
+
+        await browser.close();   
+    } catch (e) {
+        console.log("CAUGHT AN ERROR OPENING THE PAGE", e)
     }
-
-    await browser.close();
 }
